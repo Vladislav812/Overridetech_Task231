52993275af78c024960b3b1f610991bc7779d818
Vladislav812 <bolotin_vlad@list.ru>
2024-04-02 23:22:34 +0300

    opt pipeline-17

--- src/main/java/overridetech/task231/controller/PrepopulateController.java
+++ src/main/java/overridetech/task231/controller/PrepopulateController.java
@@ -3,18 +3,23 @@ package overridetech.task231.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import overridetech.task231.service.UserService;
 
-@Controller
+@RestController
 public class PrepopulateController {
     @Autowired
     UserService userService;
     boolean prepopulated;
 
     @GetMapping("/prepop")
-    public void prepopulateController() {
-        if (!prepopulated) userService.prepopulateDB();
-        else return;
+    public String prepopulateController() {
+        if (!prepopulated) {
+            userService.prepopulateDB();
+            return "Prepopulation performed!";
+        } else {
+            return "Prepopulation has been already done!";
+        }
     }
 
 }
